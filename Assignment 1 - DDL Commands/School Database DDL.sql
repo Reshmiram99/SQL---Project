cef9ce7e6fdbe28c8d8943091f431e4920978019
Reshmiram99 <urc.reshmi99@gmail.com>
2024-12-25 15:21:00 +0300

    Add files via upload

---
+++ Assignment 1 - DDL Commands/School Database DDL.sql
@@ -0,0 +1,31 @@
+#1. Creating database
+CREATE DATABASE School;
+USE School;
+CREATE TABLE STUDENT (
+Roll_No INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Marks INT,
+    Grade CHAR(1)
+);
+INSERT INTO STUDENT (Roll_No, Name, Marks, Grade) VALUES
+(1, 'Alice', 85, 'A'),
+(2, 'Bob', 78, 'B'),
+(3, 'Charlie', 92, 'A');
+SELECT * FROM STUDENT;
+ALTER TABLE STUDENT
+ADD Contact VARCHAR(15);
+
+desc student;
+ALTER TABLE STUDENT
+DROP COLUMN Grade;
+
+desc student;
+
+RENAME TABLE STUDENT TO CLASSTEN;
+SHOW TABLES;
+TRUNCATE TABLE CLASSTEN;
+SELECT * FROM CLASSTEN;
+DROP TABLE CLASSTEN;
+SHOW TABLES;
+ 
+
